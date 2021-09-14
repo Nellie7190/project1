b5067a63c01e9159781ba4f9b37c4cfa2e3294d2
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-09-14 15:31:10 -0400

    js updated and hidden key

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-alert('working');
+const key = config.SECRET_API_KEY;
+console.log(key)
+
+/////functional part of page
+
+const $p = $('#p');
+
+const getRandomJoke = () => {
+//connect api with AJAX
+    $.ajax({
+        method: 'GET',
+  url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+  headers: {
+    'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
+    'x-rapidapi-key': key
+},
+        
+    }).then(
+        (data) => {
+        console.log(data);
+        // render();
+        const jokeSetup = (data.body[0].setup);
+        const jokePunchline = (data.body[0].punchline)
+    });
+}
+
+
+//create working button to retrieve joke
+$('#get-joke').on('click', getRandomJoke())
+
+// function render(data) {
+//     p
+// }
+
+
+
+/////styling part of page
+
+
